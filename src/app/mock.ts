92cf94ea7f41ba905de99a208a081703261d67ce
Alexandr <valverin95@gmail.com>
2020-04-17 22:24:32 +0300

    feat: Gallery - extend template

--- src/app/mock.ts
+++ src/app/mock.ts
@@ -1,38 +1,50 @@
 export const GALLERY_ITEMS = [
     {
-      src: 'https://colorlib.com/preview/theme/bitmap/img/gallery/gallery-1.jpg'
+        title: 'Spreading Peace to world',
+        src: 'https://colorlib.com/preview/theme/bitmap/img/gallery/gallery-1.jpg'
     },
     {
-      src: 'https://colorlib.com/preview/theme/bitmap/img/gallery/gallery-2.jpg'
+        title: 'Spreading Peace to world',
+        src: 'https://colorlib.com/preview/theme/bitmap/img/gallery/gallery-2.jpg'
     },
     {
-      src: 'https://colorlib.com/preview/theme/bitmap/img/gallery/gallery-3.jpg'
+        title: 'Spreading Peace to world',
+        src: 'https://colorlib.com/preview/theme/bitmap/img/gallery/gallery-3.jpg'
     },
     {
-      src: 'https://colorlib.com/preview/theme/bitmap/img/gallery/gallery-4.jpg'
+        title: 'Spreading Peace to world',
+        src: 'https://colorlib.com/preview/theme/bitmap/img/gallery/gallery-4.jpg'
     },
     {
-      src: 'https://colorlib.com/preview/theme/bitmap/img/gallery/gallery-5.jpg'
+        title: 'Spreading Peace to world',
+        src: 'https://colorlib.com/preview/theme/bitmap/img/gallery/gallery-5.jpg'
     },
     {
-      src: 'https://colorlib.com/preview/theme/bitmap/img/gallery/gallery-6.jpg'
+        title: 'Spreading Peace to world',
+        src: 'https://colorlib.com/preview/theme/bitmap/img/gallery/gallery-6.jpg'
     },
     {
-      src: 'https://colorlib.com/preview/theme/bitmap/img/gallery/gallery-7.jpg'
+        title: 'Spreading Peace to world',
+        src: 'https://colorlib.com/preview/theme/bitmap/img/gallery/gallery-7.jpg'
     },
     {
-      src: 'https://colorlib.com/preview/theme/bitmap/img/gallery/gallery-8.jpg'
+        title: 'Spreading Peace to world',
+        src: 'https://colorlib.com/preview/theme/bitmap/img/gallery/gallery-8.jpg'
     },
     {
-      src: 'https://colorlib.com/preview/theme/bitmap/img/gallery/gallery-9.jpg'
+        title: 'Spreading Peace to world',
+        src: 'https://colorlib.com/preview/theme/bitmap/img/gallery/gallery-9.jpg'
     },
     {
-      src: 'https://colorlib.com/preview/theme/bitmap/img/gallery/gallery-10.jpg'
+        title: 'Spreading Peace to world',
+        src: 'https://colorlib.com/preview/theme/bitmap/img/gallery/gallery-10.jpg'
     },
     {
-      src: 'https://colorlib.com/preview/theme/bitmap/img/gallery/gallery-11.jpg'
+        title: 'Spreading Peace to world',
+        src: 'https://colorlib.com/preview/theme/bitmap/img/gallery/gallery-11.jpg'
     },
     {
-      src: 'https://colorlib.com/preview/theme/bitmap/img/gallery/gallery-12.jpg'
+        title: 'Spreading Peace to world',
+        src: 'https://colorlib.com/preview/theme/bitmap/img/gallery/gallery-12.jpg'
     },
-  ]
+]
